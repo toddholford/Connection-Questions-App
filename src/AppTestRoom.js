9e5026f35129eb4de44bc364ec01ad5cf2d75413
Todd Holford <toddholford1996@gmail.com>
2022-02-13 18:49:06 -0600

    -added working messaging through a submit form -data is showing in cloud firestore when form is submitted -added config folder with firebaseConfig.js file to store app initialization

--- src/AppTestRoom.js
+++ src/AppTestRoom.js
@@ -1,162 +1,126 @@
-﻿import React, {useRef, useState} from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes
-} from "react-router-dom";
-import { MDBContainer } from 'mdb-react-ui-kit';
-import firebase from "firebase/compat/app";
-import {doc, collection, addDoc, getFirestore, documentId} from "firebase/firestore";
-import { initializeApp } from "firebase/app"
-import { GenerateLobbyCode } from "./js/LobbyCodeGenerator"
+﻿import React, {useState} from "react";
 
-async function AppTestRoom() {
+import firebase from "./config/firebase";
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
 
-    // const writeNewLobby = async (e) => {
-    //
-    //     const docRef = addDoc(collection(db, "Lobbies"), {
-    //         lobbyCode: GenerateLobbyCode(),
-    //         lobbyCreator: "Bobby J",
-    //         lobbyJoiner: ""
-    //     });
-    //     collection(db, {docRef} + "/LobbyQuestions");
-    // }
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
-    // doc.ref.collection('LobbyQuestions')
+// Needed for User Auth
+// const auth = firebase.auth();
 
+// Possible analytics from firebase
+// const analytics = firebase.analytics();
 
-    // QUERY TO DELETE LOBBIES
-    // const lobbies_query = db.collection('Lobbies').where('lobbyCode','!=',1234);
-    // lobbies_query.get().then(function(querySnapshot) {
-    //     querySnapshot.forEach(function(doc) {
-    //         doc.ref.delete().then(r => {});
-    //     });
-    // });
+const firestore = firebase.firestore();
+
+function AppTestRoom() {
+
+    // Needed for User Auth
+    // const [user] = useAuthState(auth);
     
     return (
-        <div>
-            <h1>React & Firebase</h1>
-            <h2>By @farazamiruddin</h2>
-            {/*<button onClick={await lobbies_query} type={"button"}></button>*/}
+        <div className="App">
+            <header>
+                
+            </header>         
+            
+            <section>
+                {/*How to switch rooms based on if user has signed in*/}
+                {/*{user ? <PracticeLobbyRoom /> : <SignIn />}*/}
+                <PracticeLobbyRoom />
+            </section>
         </div>
     );
 
-    // const [user] = useAuthState(auth);
-    // return (
-    //     <MDBContainer>
-    //         <Router>
-    //             <Routes>
-    //                 <Route path="/create" element={<CreateRoom />} />
-    //                 {/*<Route path="/join" element={<JoinRoom />} />*/}
-    //                 {/*<Route path="/lobby:id" element={<LobbyRoom />} />*/}
-    //                 {/*<Route path="/" element={<ConnectRoom />} />*/}
-    //                 <Route path="/" element={<TestRoom1 />} />
-    //             </Routes>
-    //         </Router>
-    //     </MDBContainer>
-    // );
 }
 
+function PracticeLobbyRoom() {
 
+    // used to scrolled down
+    // const dummy = useRef();
 
+    const messagesRef = firestore.collection('Lobbies')
+    const query = messagesRef.orderBy('createdAt').limit(25);
+    
+    const [messages] = useCollectionData(query, {idField: 'id'});
+    
+    const [formValue, setFormValue] = useState('');
+    
+    const sendMessage = async(e) => {
+        //keeps the page from refreshing
+        e.preventDefault();
+        
+        // Needed for User Auth
+        // const { uid } = auth.currentUser;
 
+        await messagesRef.add({
+           text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
+        });
+        
+        setFormValue('');
+        
+        //used to auto scroll down
+        // dummy.current.scrollIntoView({ behavior: 'smooth' });
+    }
+    
+    return(
+        <>
+            <div>
+                {messages && messages.map(msg => <PracticeQuestionnaireResponseBox key={msg.id} message={msg}/>)}
 
+                {/*used to auto scroll down*/}
+                {/*<span ref={dummy}></span>*/}
 
+            </div>
 
+            <form onSubmit={sendMessage}>
+                <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
+                <button type="submit">Click Me</button>
+            </form>
+        </>
+    )
+    
+}
 
+function PracticeQuestionnaireResponseBox(props) {
+    
+    const { text } = props.message;
 
+    // used to identify sent or received messages
+    // const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
+    return (
+        // alternate div for using sent or received message class
+        // <div className={`message ${messageClass}`}>
+        <div>
+            <p>{text}</p>
+        </div>
+    )
+    
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//sign in will be apart of connectroom or createroom
 // function SignIn() {
-//     signInAnonymously(auth)
-//         .then(() => {
-//             console.log("sign in")
-//         })
-//         .catch((error) => {
-//             const errorCode = error.code;
-//             const errorMessage = error.message;
-//             console.log("error signing in")
-//         });
 //
-//     return(
-//         <button onClick={signInAnonymously}>Sign in anonymously</button>
-//     )
-// }
-
-//need to figure out how to set signout upon closing site
-// function SignOut() {
-//     return auth.currentUser && (
-//         auth.signOut()
-//     )
-// }
-
-//chatroom is actually lobbyroom
-// function ChatRoom() {
-//    
-//     const lobbiesRef = firestore.collection('Lobbies');
-//
-//     const sendMessage = async(e) => {
-//         const { uid } = auth.currentUser;
-//
-//         await lobbiesRef.add({
-//             // lobbyCode: GenerateLobbyCode(),
-//             lobbyCode: 1234,
-//             // lobbyCreatorId: uid,
-//             lobbyCreator: "Todd",
-//             lobbyJoiner: ""
-//         });
-//         console.log("createLobby ran")
+//     const signInWithGoogle = () => {
+//         const provider = new firebase.auth.GoogleAuthProvider();
+//         auth.signInWithPopup(provider);
 //     }
 //
-//     return(
+//     return (
 //         <>
-//             <div>
-//                 {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
-//
-//                 <div ref={dummy}></div>
-//
-//             </div>
-//
-//             <form onSubmit={sendMessage}>
-//
-//                 <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
-//
-//                 <button type={"submit"}>click</button>
-//
-//             </form>
+//             <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+//             <p>Do not violate the community guidelines or you will be banned for life!</p>
 //         </>
 //     )
+//
 // }
-
-//chatmessage will be questionnaireresponsebox or questionnaireresponse or both
-// function ChatMessage(props) {
-//     const { text, uid } = props.message;
 //
-//     const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
-//
-//     return (
-//         <div className={`message ${messageClass}`}>
-//             <p>{text}</p>
-//         </div>
+// function SignOut() {
+//     return auth.currentUser && (
+//         <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
 //     )
 // }
 
